1cb02e126fb8fa0b12ea6425f5bb0c312dd22266
Scaro <123261052+RipAngelhacks@users.noreply.github.com>
2025-09-06 14:31:25 -0400

    Update shimboot-oobeskip.sh

--- shimboot-oobeskip.sh
+++ shimboot-oobeskip.sh
@@ -3,7 +3,7 @@
 echo "Welcome to oobeskip for shimboot"
 echo "This script allows you to skip the oobe, and get into a temporary unenrolled environment without needing to actually unenroll."
 rm '/home/chronos/Local State'
-cp /bootloader/opt/oobeskip_state '/home/chronos/Local State'
+cp /bootloader/oobeskip_state '/home/chronos/Local State'
 read -p "Do you want to restart the ui now? (y/n) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
